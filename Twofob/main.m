4b402359b59ed7e102beb3db5ed551166eca3915
Zachary Waldowski <zwaldowski@gmail.com>
2013-12-06 05:49:36 -0500

    Reoganize project

---
+++ Twofob/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Twofob
+//
+//  Created by Zach Waldowski on 11/17/13.
+//  Copyright (c) 2013 Pandamonia LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OTPAuthAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OTPAuthAppDelegate class]));
+    }
+}
